50fcf0066e989a1135db9aeead85f590c78bc647
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-09-19 14:23:49 +0700

    commit

---
+++ app/api/cafe-get/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+const coffee_menu = [
+  {
+    name: "Coffee 1",
+    price: "5$",
+    description: "",
+  },
+  {
+    name: "Coffee 2",
+    price: "6$",
+    description: "",
+  },
+  {
+    name: "Coffee 3",
+    price: "7$",
+    description: "",
+  },
+];
+export async function GET() {
+  try {
+    // let {} = await req.json();
+    return NextResponse.json(coffee_menu);
+  } catch (error) {
+    console.log("[ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
